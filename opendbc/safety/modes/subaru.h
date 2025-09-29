df4476d16ed0fbf956fb42a3e74650056dfbd8b8
kyleboyer <kyleboyer@comcast.net>
2025-09-28 21:36:06 -0500

    Var fix

--- opendbc/safety/modes/subaru.h
+++ opendbc/safety/modes/subaru.h
@@ -201,9 +201,9 @@ static bool subaru_tx_hook(const CANPacket_t *msg) {
   }
 
   if (addr == MSG_SUBARU_ES_LKAS_ANGLE) {
-    int desired_angle = GET_BYTES(to_send, 5, 3) & 0x1FFFFU;
+    int desired_angle = GET_BYTES(msg, 5, 3) & 0x1FFFFU;
     desired_angle = -1 * to_signed(desired_angle, 17);
-    bool lkas_request = GET_BIT(to_send, 12U);
+    bool lkas_request = GET_BIT(msg, 12U);
 
     violation |= steer_angle_cmd_checks(desired_angle, lkas_request, SUBARU_ANGLE_STEERING_LIMITS);
   }
