d22b6f9073326425f9da4d1bf22f60651f016119
Kyle Boyer <noreply@github.com>
2025-09-30 21:17:31 -0500

    Corrected SUBARU_LKAS_ANGLE_RX_CHECKS

--- opendbc/safety/modes/subaru.h
+++ opendbc/safety/modes/subaru.h
@@ -73,11 +73,11 @@
   {.msg = {{MSG_SUBARU_ES_LKAS_State,   SUBARU_CAM_BUS,  8, 10U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  \
 
 #define SUBARU_LKAS_ANGLE_RX_CHECKS(alt_bus)                                                                            \
-  {.msg = {{MSG_SUBARU_Throttle,        SUBARU_MAIN_BUS, 8, .max_counter = 15U, .frequency = 100U}, { 0 }, { 0 }}}, \
-  {.msg = {{MSG_SUBARU_Steering_Torque, SUBARU_MAIN_BUS, 8, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},  \
-  {.msg = {{MSG_SUBARU_Wheel_Speeds,    alt_bus,         8, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},  \
-  {.msg = {{MSG_SUBARU_Brake_Status,    alt_bus,         8, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},  \
-  {.msg = {{MSG_SUBARU_ES_DashStatus,   SUBARU_CAM_BUS,  8, .max_counter = 15U, .frequency = 20U}, { 0 }, { 0 }}},  \
+  {.msg = {{MSG_SUBARU_Throttle,        SUBARU_MAIN_BUS, 8, 100U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}}, \
+  {.msg = {{MSG_SUBARU_Steering_Torque, SUBARU_MAIN_BUS, 8, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  \
+  {.msg = {{MSG_SUBARU_Wheel_Speeds,    alt_bus,         8, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  \
+  {.msg = {{MSG_SUBARU_Brake_Status,    alt_bus,         8, 50U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  \
+  {.msg = {{MSG_SUBARU_ES_DashStatus,   SUBARU_CAM_BUS,  8, 20U, .max_counter = 15U, .ignore_quality_flag = true}, { 0 }, { 0 }}},  \
 
 static bool subaru_gen2 = false;
 static bool subaru_longitudinal = false;
@@ -126,13 +126,15 @@ static void subaru_rx_hook(const CANPacket_t *msg) {
   if (subaru_lkas_angle) {
     // LKAS Angle cars use different message
     if ((msg->addr == MSG_SUBARU_ES_DashStatus) && (msg->bus == SUBARU_CAM_BUS)) {
-      bool cruise_engaged = GET_BIT(msg, 36U);
+      // bool cruise_engaged = GET_BIT(msg, 36U);
+      bool cruise_engaged = (msg->data[4] >> 4) & 1U;
       pcm_cruise_check(cruise_engaged);
       acc_main_on = GET_BIT(msg, 40U);
     }
   } else {
     if ((msg->addr == MSG_SUBARU_CruiseControl) && (msg->bus == alt_main_bus)) {
-      bool cruise_engaged = GET_BIT(msg, 41U);
+      // bool cruise_engaged = GET_BIT(msg, 41U);
+      bool cruise_engaged = (msg->data[5] >> 1) & 1U;
       pcm_cruise_check(cruise_engaged);
       acc_main_on = GET_BIT(msg, 40U);
     }
